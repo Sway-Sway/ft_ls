34cec7a128f7f89d1698692eaa869466c768cff1
Sway-Sway <j.kwayiba@icloud.com>
2019-07-29 14:10:05 +0200

    more updates to ls

---
+++ ls/ft_ls.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkwayiba <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/29 11:01:24 by jkwayiba          #+#    #+#             */
+/*   Updated: 2019/07/29 14:09:06 by jkwayiba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LS_H
+# define FT_LS_H
+# include <stdlib.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <dirent.h>
+# include "libft/libft.h"
+# include <time.h>
+# include <pwd.h>
+# include <grp.h>
+
+#endif
--- ls/v1.c
+++ ls/v1.c
@@ -1,19 +1,28 @@
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <dirent.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   v1.c                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkwayiba <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/29 11:02:04 by jkwayiba          #+#    #+#             */
+/*   Updated: 2019/07/29 14:08:17 by jkwayiba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ls.h"
 
 int		main(int ac, char **av)
 {
 	DIR *dir;
 	struct	dirent	*sd;
 	struct	stat	fileStat;
+	struct 	passwd	*pass;
+	struct	group	*grpss;
 	char	fullpath[256];
 
-	dir = opendir(av[1]);
 
+	dir = opendir(av[1]);
 	if(dir == NULL)
 	{
 		printf("Error! Unable to open directory.\n");
@@ -26,6 +35,8 @@ int		main(int ac, char **av)
 		strcat (fullpath, sd->d_name);
 		if(!stat(fullpath, &fileStat))
 		{
+			pass = getpwuid(fileStat.st_uid);
+			grpss = getgrgid(fileStat.st_gid);
 			printf((S_ISDIR(fileStat.st_mode)) ? "d" : "-");
 			printf((fileStat.st_mode & S_IRUSR) ? "r" : "-");
 			printf((fileStat.st_mode & S_IWUSR) ? "w" : "-");
@@ -36,7 +47,8 @@ int		main(int ac, char **av)
 			printf((fileStat.st_mode & S_IROTH) ? "r" : "-");
 			printf((fileStat.st_mode & S_IWOTH) ? "w" : "-");
 			printf((fileStat.st_mode & S_IXOTH) ? "x" : "-");
-
+			printf(" %s", pass->pw_name);
+			printf(" %s", grpss->gr_name);
 		}
 		printf(" %s\n", sd->d_name);
 	}
