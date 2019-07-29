92d48bbd539ec1c0d1e9b6e0d1118cf79f1c1f03
Sway-Sway <j.kwayiba@icloud.com>
2019-07-29 10:56:53 +0200

    update to ls

--- ls/v1.c
+++ ls/v1.c
@@ -1,12 +1,16 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <dirent.h>
 
 int		main(int ac, char **av)
 {
 	DIR *dir;
 	struct	dirent	*sd;
+	struct	stat	fileStat;
+	char	fullpath[256];
 
 	dir = opendir(av[1]);
 
@@ -17,7 +21,24 @@ int		main(int ac, char **av)
 	}
 	while((sd=readdir(dir)) != NULL)
 	{
-		printf("%s\n", sd->d_name);
+		strcpy (fullpath, av[1]);
+		strcat (fullpath, "/");
+		strcat (fullpath, sd->d_name);
+		if(!stat(fullpath, &fileStat))
+		{
+			printf((S_ISDIR(fileStat.st_mode)) ? "d" : "-");
+			printf((fileStat.st_mode & S_IRUSR) ? "r" : "-");
+			printf((fileStat.st_mode & S_IWUSR) ? "w" : "-");
+			printf((fileStat.st_mode & S_IXUSR) ? "x" : "-");
+			printf((fileStat.st_mode & S_IRGRP) ? "r" : "-");
+			printf((fileStat.st_mode & S_IWGRP) ? "w" : "-");
+			printf((fileStat.st_mode & S_IXGRP) ? "x" : "-");
+			printf((fileStat.st_mode & S_IROTH) ? "r" : "-");
+			printf((fileStat.st_mode & S_IWOTH) ? "w" : "-");
+			printf((fileStat.st_mode & S_IXOTH) ? "x" : "-");
+
+		}
+		printf(" %s\n", sd->d_name);
 	}
 	closedir(dir);
 	return (0);
